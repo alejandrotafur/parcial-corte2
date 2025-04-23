4696fec399a241fc5804d652761679431592a7e6
Alejandro Tafur jojoa <atafur-2023a@corhuila.edu.co>
2025-04-22 22:51:08 -0500

    feat: Componente de selección de mesa e integración en pantalla principal

--- app/reserva-restaurante/src/pages/Home.tsx
+++ app/reserva-restaurante/src/pages/Home.tsx
@@ -1,25 +1,18 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import React from 'react';
 
-const Home: React.FC = () => {
+import TableSelector from '../components/TableSelector';
+
+const Home = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
+    <div style={{ padding: '20px' }}>
+      <h1>Gestión de Reservas</h1>
+
+      <section>
+        <TableSelector />
+      </section>
+    </div>
   );
 };
 
 export default Home;
+
