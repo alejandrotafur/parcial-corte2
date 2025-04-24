0883e75e4c9479396ea2a7ac8755e96e75cf3c8d
Alejandro Tafur jojoa <atafur-2023a@corhuila.edu.co>
2025-04-24 04:01:40 -0500

    Remove frontend files from feat/HU-5

--- app/reserva-restaurante/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'reserva-restaurante',
-  webDir: 'dist'
-};
-
-export default config;
